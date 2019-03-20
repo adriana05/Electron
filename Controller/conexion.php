be43745f2b3598764d214c7c5d2aec3e3c6af131
adriana05 <adrianaelibethqr@ufps.edu.co>
2019-03-20 11:40:01 -0500

    se añade trabajo electron

---
+++ Controller/conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+$hostname_localhost = "localhost";
+$database_localhost = "concesionaria";
+$username_localhost = "root";
+$password_localhost = "";
+
+mysqli_connect($localhost,$username_localhost,$password_localhost,$database_localhost) or die("Error al conectarse al servidor");
+?>
